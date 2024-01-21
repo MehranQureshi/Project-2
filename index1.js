222ffa04a22cd6d095d7d15e3cae0d635d03f5d6
MehranQureshi <157192393+MehranQureshi@users.noreply.github.com>
2024-01-21 14:55:54 +0500

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,18 @@
+let videoContainer=document.querySelector(".video-container");
+let switchBtn=document.querySelector(".switch-btn");
+switchBtn.addEventListener('click',()=>{
+    if(!switchBtn.classList.contains('slide')){
+    switchBtn.classList.add('slide');
+    videoContainer.pause();
+}
+else{
+    switchBtn.classList.remove('slide')
+    videoContainer.pause();
+}  
+})
+let preloader=document.querySelector(".preloader");
+window.addEventListener('load',()=>{
+    if(!preloader.classList.contains("hide-preloader")){
+    preloader.classList.add("hide-preloader")
+}
+})
